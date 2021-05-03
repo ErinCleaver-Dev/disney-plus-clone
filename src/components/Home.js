9afed7ba29b07b8900399546627a9e0834833206
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2021-05-03 16:26:27 -0400

    Added information to Home page
    Disney Clone

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,8 +13,19 @@ const Home = () => {
 
 export default Home
 
-const Container = styles.div`
-    height: 70px;
-    background: #0A0B11;
-    color: white;
+const Container = styles.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+
+    &: before { 
+        background: url("/images/home-background.png") center center / cover no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0; 
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
 `
