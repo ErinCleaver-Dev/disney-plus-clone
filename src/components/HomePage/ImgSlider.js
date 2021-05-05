71d283f8e3b9704485b719da2f97d708cd3b278c
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2021-05-05 18:04:11 -0400

    Update

--- src/components/HomePage/ImgSlider.js
+++ src/components/HomePage/ImgSlider.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import * as  Slider_Images from '../../Data/slider_images.json';
+import * as  Images from '../../Data/slider_images.json';
 
 const ImgSlider = () => {
 
@@ -20,7 +20,7 @@ const ImgSlider = () => {
         // added a container to contain the information for the image silder
         <Carousel {...settings}>
             {
-                Slider_Images.Slider_Images.map((image) =>(
+                Images.Slider_Images.map((image) =>(
                     <Wrap key={image.id}>
                         <img src={image.url}/>
                     </Wrap>
@@ -33,7 +33,35 @@ const ImgSlider = () => {
 export default ImgSlider
 
 const Carousel = styled(Slider)`
+    margin-top: 20px;
 
+    ul li button {
+        &:before {
+            font-size: 10px;
+            color: rgb(150, 158, 171);
+        }
+    }
+
+    li.slick-active button::before {
+        color: white;
+    }
+
+    .slick-list {
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+    }
 `
 const Wrap = styled.div`
+
+    img {
+        border: 4px solid transparent;
+        border-radius: 4px;
+        width: 100%;
+        height: 100%;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    }
 `
