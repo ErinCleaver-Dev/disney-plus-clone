bf8462e02984812b654a97160369fb8a34eef1b0
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-05-10 08:36:40 -0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from 'styled-components'
-import * as Links from '../Data/Links.json'
 
 const Header = () => {
     return (
@@ -8,15 +7,30 @@ const Header = () => {
         <Nav>
             <Logo src="/images/logo.svg" />
             <NavMenu>
-
-                {
-                    Links.Links.map((link) =>(
-                        <a href="#" key={link.id}>
-                        <img src={link.icon} alt={link.alt} />
-                        <span>{link.title}</span>
-                        </a>
-                    ))
-                }
+                <a href="#">
+                    <img src="/images/home-icon.svg" alt="Home Icon" />
+                    <span>HOME</span>
+                </a>
+                <a href="#">
+                    <img src="/images/search-icon.svg" alt="Search Icon" />
+                    <span>SEARCH</span>
+                </a>
+                <a href="#">
+                    <img src="/images/watchlist-icon.svg" alt="Watchlist Icon" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href="#">
+                    <img src="/images/original-icon.svg" alt="Original Icon" />
+                    <span>ORIGINAL</span>
+                </a>
+                <a href="#">
+                    <img src="/images/movie-icon.svg" alt="Movie Icon" />
+                    <span>MOVIES</span>
+                </a>
+                <a href="#">
+                    <img src="/images/series-icon.svg" alt="Series Icon" />
+                    <span>SERIES</span>
+                </a>
                 
             </NavMenu>
             <UserImage src="/images/Yoda.jpeg"/>
@@ -32,7 +46,6 @@ const Nav = styles.nav`
     display: flex;
     align-items: center;
     padding: 0 36px;
-    overflow-x: hidden;
 `
 
 // Added a logo const variable for the nav bar that is equal to styles.img
--- src/components/HomePage/ImgSlider.js
+++ src/components/HomePage/ImgSlider.js
@@ -56,7 +56,7 @@ const Carousel = styled(Slider)`
     }
 `
 const Wrap = styled.div`
-    cursor: pointer;
+
     img {
         border: 4px solid transparent;
         border-radius: 4px;
@@ -64,10 +64,5 @@ const Wrap = styled.div`
         height: 100%;
         box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
         rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-        transition-duration: 300ms;
-
-        &:hover {
-            border: 4px solid rgba(249, 249, 249, 0.8);
-        }
     }
 `
